5ed182229c30cadd1e163be9ceec06c4d241f7a2
t-yorozu <62528223+t-yorozu@users.noreply.github.com>
2020-06-23 10:53:11 +0900

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,6 +1,28 @@
-<?php
+<?php<?php
 $dsn = 'mysql:dbname=sample_db;host=localhost;';
 $user = 'yorozu';
+$password = 'morijyobi';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+
+    $sql = "insert into user values (:id, :name, :age)";
+    $stmt = $dmk->prepare($sql);
+    $params = array(':id' => $id, ':name' => $name, ':age' => $age);
+    $stmt->execute($params);
+
+
+    header('location: index.php?fg=1');
+} catch (PDOException $e) {
+    header('location: index.php?fg=1?err=$e->getMessage');
+    //echo "接続失敗: " . $e->getMessage() . "\n";
+    exit();
+}
+?>
 $password = 'morijyobi';
 try {
     $dbh = new PDO($dsn, $user, $password);
